7f62070a201f02489b7bd2c60423e86a21c16d9b
homa0401 <humoyun0401@gmail.com>
2022-11-04 16:24:16 +0900

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = `7875bf9a-9b0b-4c7a-aab0-265bce41735b`;
+export const MODAL_CLOSE_SEC = 2.5;
